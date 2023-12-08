2dfbaf3517a1eac76bd1f9df4e9ecf4154305a9e
bigkaiyoh <rbc_kaiyoh@gmail.com>
2023-12-08 13:48:46 +0900

    modified Home.py

--- Home.py
+++ Home.py
@@ -155,7 +155,7 @@ def main():
     #Get user input
     user_input = get_user_input(style)
 
-    submit_button = st.button(translate("採点する", "Grade it!", JP)
+    submit_button = st.button(translate("採点する", "Grade it!", JP))
     if submit_button:
         if style == "Speaking":
             # Transcribe audio
