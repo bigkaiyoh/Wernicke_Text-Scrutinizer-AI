fc61d77f678fd1bbeab03045ed5951a29296849d
bigkaiyoh <rbc_kaiyoh@gmail.com>
2023-12-13 23:02:13 +0900

    modified "Home.py"

--- Home.py
+++ Home.py
@@ -2,7 +2,6 @@ import streamlit as st
 from st_paywall import add_auth
 from openai import OpenAI
 from streamlit_gsheets import GSheetsConnection
-from streamlit_extras.app_logo import add_logo
 import pandas as pd
 import time
 
