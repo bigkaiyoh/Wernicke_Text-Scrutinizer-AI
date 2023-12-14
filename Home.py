4f3851554828ac2f2e3d2880e9a44a9399a77ec4
bigkaiyoh <rbc_kaiyoh@gmail.com>
2023-12-15 00:00:48 +0900

    modified "Home.py"

--- Home.py
+++ Home.py
@@ -31,7 +31,7 @@ st.set_page_config(
 #Removing Hooter and Footer
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
+            #MainMenu {visibility: visible;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
