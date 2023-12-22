144b79b9991cbd8ec439972d54ddaabab1220381
bigkaiyoh <rbc_kaiyoh@gmail.com>
2023-12-22 21:46:40 +0900

    mody

--- Home.py
+++ Home.py
@@ -369,15 +369,17 @@ def main():
                 no_input_error(JP)
 
         # Handling the translation
+        translation_button_placeholder = st.empty()
         if st.session_state.submit_clicked and not st.session_state.translation_completed:
             if 'evaluation' in st.session_state:
-                if st.button(translate("日本語に翻訳", "Translate Feedback to Japanese", JP), key="deepl"):
+                if translation_button_placeholder.button(translate("日本語に翻訳", "Translate Feedback to Japanese", JP), key="deepl"):
                     temporary.empty()
                     # Translate the evaluation
                     translated_text = deepl_translation(st.session_state.evaluation, "JA")
                     st.session_state.translated_evaluation = translated_text
                     st.session_state.translation_completed = True
             if st.session_state.translation_completed:
+                translation_button_placeholder.empty()
                 user_message = st.chat_message("user")
                 user_message.write(user_input)
                 translated_message = st.chat_message("assistant")
