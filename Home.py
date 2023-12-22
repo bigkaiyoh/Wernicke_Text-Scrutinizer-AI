65c807d64c1c0356793ff8c565626350003cad49
bigkaiyoh <rbc_kaiyoh@gmail.com>
2023-12-22 21:19:08 +0900

    modified "Home.py"

--- Home.py
+++ Home.py
@@ -371,19 +371,18 @@ def main():
 
         # Handling the translation
         if st.session_state.submit_clicked and not st.session_state.translation_completed:
-            if st.button(translate("日本語に翻訳", "Translate Feedback to Japanese", JP), key="deepl"):
-                temporary.empty()
-                # Translate the evaluation
-                translated_text = deepl_translation(st.session_state.evaluation, "JA")
-                st.session_state.translated_evaluation = translated_text
-                st.session_state.translation_completed = True
-                # Display the translated evaluation using chat UI
-                user_message = st.chat_message("user")
-                user_message.write(user_input)
-                translated_message = st.chat_message("assistant")
-                translated_message.write(translated_text)
-            elif 'evaluation' in st.session_state and not st.session_state.evaluation:
-                st.error("No evaluation to translate.")
+            if 'evaluation' in st.session_state:
+                if st.button(translate("日本語に翻訳", "Translate Feedback to Japanese", JP), key="deepl"):
+                    temporary.empty()
+                    # Translate the evaluation
+                    translated_text = deepl_translation(st.session_state.evaluation, "JA")
+                    st.session_state.translated_evaluation = translated_text
+                    st.session_state.translation_completed = True
+                    # Display the translated evaluation using chat UI
+                    user_message = st.chat_message("user")
+                    user_message.write(user_input)
+                    translated_message = st.chat_message("assistant")
+                    translated_message.write(translated_text)
 
 
     #Question Chat Box
