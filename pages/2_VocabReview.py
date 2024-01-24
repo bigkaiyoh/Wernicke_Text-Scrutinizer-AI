5caee3a75b00e291367fcdeb49fd0b147803d9a8
bigkaiyoh <rbc_kaiyoh@gmail.com>
2024-01-24 20:33:01 +0900

    clear "text_input"

--- pages/2_VocabReview.py
+++ pages/2_VocabReview.py
@@ -90,22 +90,34 @@ def main():
         words = fetch_user_words(st.query_params['user'], JP)
         if words:
             print_words(words, JP)
+
+            # Use a common state variable for both text inputs
+            if 'word_input' not in st.session_state:
+                st.session_state.word_input = ''
             c1, c2 = st.columns(2)
             with c1:
-                added_word = st.text_input("Enter a word to add 👇", key="new_word")
+                added_word = st.text_input("Enter a word to add 👇", 
+                                           key="word_input", 
+                                           value=st.session_state.word_input
+                                           )
                 if st.button("Add Word", key="add_word"):
                     if add_word_to_sheet(st.query_params['user'], added_word):
                         st.session_state['added_success'] = True
+                        st.session_state.word_input = ''
                         st.rerun()
                 # Handle success message
                 if st.session_state.get('added_success', False):
                     st.success("Word added successfully!")
                     st.session_state['added_success'] = False
             with c2:
-                deleted_word = st.text_input("Enter a word to delete 👇", key="delete_word")
+                deleted_word = st.text_input("Enter a word to delete 👇", 
+                                             key="word_input", 
+                                             value=st.session_state.word_input
+                                             )
                 if st.button("Delete Word", key="delete_word_btn"):
                     if delete_word_from_sheet(st.query_params['user'], deleted_word):
                         st.session_state['deleted_success'] = True
+                        st.session_state.word_input = ''
                         st.rerun()
                 # Handle success message
                 if st.session_state.get('deleted_success', False):
