ffb246a509559137e5d2b28b92617b6ab3abb960
bigkaiyoh <rbc_kaiyoh@gmail.com>
2024-02-11 11:51:24 +0900

    modified

--- Home.py
+++ Home.py
@@ -476,8 +476,8 @@ def main():
 
 
     if selected == translate("マイページ", "My History", JP):
-        existing_data = fetch_latest_data(conn)
-        user_data = existing_data[existing_data['user_email'] == st.session_state.email]  # Filter by email
+        latest_data = fetch_latest_data(conn)
+        user_data = latest_data[latest_data['user_email'] == st.session_state.email]  # Filter by email
         # Do not display user_email and add a date row
         display_data = user_data.drop(columns=['user_email']).copy()
         display_data['date'] = pd.to_datetime(display_data['timestamp']).dt.date
